21bde296c17badcb05eea9c5be420e6195121b17
Marcelo Vieira <39255607+SistemasVox@users.noreply.github.com>
2023-03-13 19:30:27 -0300

    Update down_rename_join_up.sh

--- down_rename_join_up.sh
+++ down_rename_join_up.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# Definir o tempo limite para a execução
-max_execution_time=0
+# Definir função de manipulador de sinais
+function on_sigint {
+    echo -e "\n\nO download foi interrompido pelo usuário."
+    exit 1
+}
+
+# Configurar o manipulador de sinais para o sinal SIGINT (Ctrl + C)
+trap on_sigint SIGINT
 
 url="https://dadosabertos.rfb.gov.br/CNPJ/"
 echo "URL: $url"
@@ -11,6 +17,13 @@ dir="zip"
 if [ ! -d "$dir" ]
 then
     mkdir "$dir"
+fi
+
+# Criar um diretório para armazenar os arquivos csv
+csv_dir="csv"
+if [ ! -d "$csv_dir" ]
+then
+    mkdir "$csv_dir"
 fi
 
 # Verificar se a URL está corretamente formatada
@@ -44,23 +57,22 @@ do
     fi
 
     # Descompactar o arquivo zip e renomear o arquivo resultante
-    if unzip -q "$filename" -d "$dir"; then
-        num=1
-        for csv_file in "$dir"/*.csv; do
-            if [[ -f "$csv_file" ]]; then
-                new_filename="${dir}/arq$(printf '%02d' $num).csv"
-                mv "$csv_file" "$new_filename"
-                ((num++))
-            fi
-        done
-    else
-        echo -e "\nErro ao abrir o arquivo $filename"
-        continue
-    fi
+	if unzip -q "$filename" -d "$csv_dir"; then
+		for csv_file in "$csv_dir"/*.ESTABELE; do
+			if [[ -f "$csv_file" ]]; then
+				new_filename="${csv_file%.ESTABELE}.csv"
+				mv "$csv_file" "$new_filename"
+			fi
+		done
+	else
+		echo -e "\nErro ao abrir o arquivo $filename"
+		continue
+	fi
+
 done
 
 # Verificar se pelo menos um arquivo csv foi descompactado
-if ! ls "$dir"/*.csv >/dev/null 2>&1; then
+if ! ls "$csv_dir"/*.csv >/dev/null 2>&1; then
     echo -e "\nErro: Não foi possível encontrar nenhum arquivo CSV para unir."
     exit 1
 fi
@@ -72,14 +84,23 @@ if [ -f "$csv_file" ]
 then
     rm "$csv_file"
 fi
-head -1 "$dir/arq01.csv" > "$csv_file"
-cat "$dir/arq*.csv" | tail -n +2 >> "$csv_file"
+find "$csv_dir" -name '*.csv' -type f -print0 | sort -z | xargs -0 head -n 1 > "$csv_file"
+tail -n +2 "$csv_dir"/*.csv >> "$csv_file"
 
-# Limpar o diretório
-echo -e "\n---- Limpando o diretório..."
-rm -f "$dir"/*.zip
-rm -f "$dir"/*.csv
+echo -e "\nArquivo $csv_file criado com sucesso!!!!"
 
-# Importar os arquivos CSV para o MySQL
-# mysqlimport --user=username --password=password --local --fields-terminated-by=';' --lines-terminated-by='\n' database nome_da_tabela "${dir}/uniao.csv"
-echo "Processo concluído!"
+# Perguntar ao usuário se ele deseja importar o arquivo csv para o banco de dados
+while true; do
+    read -p "Deseja importar o arquivo $csv_file para o banco de dados? [s/N] " sn
+    case $sn in
+        [Ss]* ) 
+        mysqlimport --user=username --password=password --local --fields-terminated-by=';' --lines-terminated-by='\n' database nome_da_tabela "${dir}/uniao.csv";
+        echo -e "\nArquivo $csv_file importado para o banco de dados com sucesso!";
+		# Excluir os diretórios zip e csv
+		echo -e "\nExcluindo diretórios zip e csv..."
+		rm -rf "$dir" "$csv_dir"
+		break;;
+        [Nn]* ) exit;;
+        * ) echo "Por favor responda sim (s) ou não (N).";;
+    esac
+done
