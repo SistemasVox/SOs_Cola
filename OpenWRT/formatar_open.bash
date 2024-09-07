8dffee44e5f813d338bcaf46897028420676edfa
Marcelo Vieira <39255607+SistemasVox@users.noreply.github.com>
2024-09-07 14:09:23 -0300

    formatar_open.bash

---
+++ OpenWRT/formatar_open.bash
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Lista os pacotes instalados e salva no arquivo pacotes_instalados.txt
+opkg list-installed > /root/home/pacotes_instalados.txt
+
+# Filtra apenas os nomes dos pacotes (sem versões) e salva no arquivo pacotes_nomes.txt
+opkg list-installed | awk '{print $1}' > /root/home/pacotes_nomes.txt
+
+# Coloca os nomes dos pacotes em uma única linha, separados por espaço, e salva no arquivo pacotes_nomes.txt
+opkg list-installed | awk '{print $1}' | tr '\n' ' ' > /root/home/pacotes_nomes_install.txt
